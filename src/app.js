e6b836e6e6d80f5abdfa1ed7307416a4a99725c4
dushyant kumar <dushyantvelar@gmail.com>
2026-02-19 10:02:07 +0530

    set up the socket manager

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { createServer } from "node:http";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectToSocket } from "./controllers/socketManager.js";
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+ 
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+const password = process.env.PASSWORD;
+
+app.set("port", process.env.PORT || 8080);
+app.use(cors());
+app.use(express.json({limit:"40kb"})); //we give limit because payload not run more
+app.use(express.urlencoded({limit:"40kb", extended:true}));
+app.use("/api/v1/users",userRoutes);
+
+
+
+
+const start = async () => {
+  app.set("mongo_user");
+  const connectionDB = await mongoose.connect(
+    `mongodb+srv://dushyantvelar:${password}@cluster0.iz1rdkn.mongodb.net/`
+  );
+  console.log(`MONGO Connected DB Host:${connectionDB.connection.host}`);
+  server.listen(app.get("port"), () => {
+    console.log("LISTENING ON PORT 8080");
+  });
+};
+start();
---
+++ src/models/meeting.model.js
@@ -0,0 +1,10 @@
+import {Schema,model} from "mongoose";
+
+const meetingSchema = new Schema({
+    user_id:{type:String,required:true},
+    meetingCode:{type:String,required:true},
+    date:{type:Date,default:Date.now,required:ture}
+});
+
+const Meeting = model(Meeting,meetingSchema);
+export {Meeting};
